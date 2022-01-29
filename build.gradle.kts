0a706f0d2f916e04df000658b19fefdd36fe5e9e
rustam <ahunjonovrustam@gmail.com>
2022-01-30 00:09:30 +0500

    fix gradele

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
-    id("maven-publish")
+    `maven-publish`
     id("org.springframework.boot") version "2.6.3"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.10"
@@ -14,17 +15,15 @@ java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
     mavenCentral()
-
 }
-
 publishing {
     repositories {
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/rustikHub/tg-bot-lib")
             credentials {
-                username = "rustikHub"
-                password = "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"
+                username =  System.getenv("GITHUB_NAME")
+                password = System.getenv("GITHUB_TOKEN")
             }
         }
     }
@@ -37,7 +36,7 @@ publishing {
 
 dependencies {
     implementation("org.telegram:telegrambots:5.4.0.1")
-    implementation ("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -50,7 +49,7 @@ tasks.withType<KotlinCompile> {
     }
 }
 
-tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+tasks.getByName<BootJar>("bootJar") {
     enabled = false
 }
 
